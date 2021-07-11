14aeafa9c5f4f98cd2fa3fc9ec2b63a5b4112efc
ARMINPAN <arminpp1379@gmail.com>
2021-07-11 21:52:05 +0430

    spike triggered stimulus extracted

--- main.m
+++ main.m
@@ -2,14 +2,15 @@
 % implemented paper : Isolation of Relevant Visual Features from Random Stimuli for
 % Cortical Complex Cells, 
 % Authors: Jon Touryan,1 Brian Lau,2 and Yang Dan1,2
-clc; clear;
-
+addpath('MatlabFunctions\fileload');
+addpath('MatlabFunctions\tview');
+close all; clc; clear;
 % part.1 - Dataset
 % sa0 files
 numberOfFrames = 32767;
-T = 1/52.721395; % s
+T = 1/59.721395; % s
 
-% all neurons` name vector
+% all neurons name vector
 nameVector = ["000412", "000413", "000418", "000419", "000420"...
     "000503", "000511", "000513", "000524", "000525", "000601"...
 "000620", "000622", "000712", "000720", "000802", "000804"...
@@ -37,14 +38,11 @@ title('spike count rate histogram','interpreter','latex');
 lessThanTwoSCRsNeuronCodes = nameVector(find(SCR<2))
 
 % load msq1D stimulus
-msq1D = load('Data\Stimulus_Files\msq1D.mat');
-stimuliExtracted = []
-for i=1:length(nameVector)
-    msq1Dstruct = Func_ReadData(nameVector(i));
-    for j=1:length(msq1Dstruct)
-        Func_StimuliExtraction(msq1Dstruct(j).events,msq1D);
-    end
-end
+msq1D = load('Data\Stimulus_Files\msq1D.mat').msq1D;
+neuronCode = "000413";
+experimentID = "b03bmsq1D";
+stimuliExtracted = Func_StimuliExtraction(neuronCode,experimentID,msq1D,T);
+
 
 %% functions
 function outputStruct = Func_ReadData(neuronCode)
@@ -71,7 +69,26 @@ function SCR = plotSpikeCountRate(neuronCode,msq1Dstruct,T,numberOfFrames)
     SCR = SCR/(length(msq1Dstruct)*T*numberOfFrames);
 end
 
-function Func_StimuliExtraction(events,msq1D)
-
-
+function stimuliExtraction = Func_StimuliExtraction(neuronCode,experimentID,msq1D,T)
+    timeSpan = 10000*16*T;
+    msq1Dstruct = Func_ReadData(neuronCode);
+    targetExperiment = 0;
+    for i = 1:length(msq1Dstruct)
+    	if(msq1Dstruct(i).hdr.DataInfo.ID == experimentID)
+            targetExperiment = i;
+            break;
+        end
+    end
+    triggered_stimulus = ceil(msq1Dstruct(targetExperiment).events/timeSpan);
+    stimuliExtraction = zeros(16,16,length(triggered_stimulus));
+    for i=1:length(triggered_stimulus)
+        stimuliExtraction(:,:,i) = msq1D(((triggered_stimulus(i)-1)*16+1):((triggered_stimulus(i))*16),:);
+    end
 end
+
+
+
+
+
+
+
